94264c6cb2e4c85c84474208f582bfe6e9992600
paulomatheus <paulomatheus.araujo@hotmail.com>
2025-10-18 14:02:40 -0300

    refactor: using ESM

---
+++ localstorage/src/app.js
@@ -0,0 +1,40 @@
+
+import HTMLService from "./HTMLService.js";
+import SubscriberService from "./SubscriberService.js";
+
+class App {
+  constructor() {
+    const subscriberService = new SubscriberService();
+    new HTMLService(subscriberService);
+    console.log(`👁️ [app.js] initialized`);
+  }
+}
+
+new App();
+
+// const form = document.querySelector("form");
+// const main = document.querySelector("main");
+
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   console.log("[app.js] form trigged!");
+//   console.log("[app.js] email:", form.email.value);
+//   window.localStorage.setItem("email", form.email.value);
+//   console.log("[app.js] email saved!");
+//   form.reset();
+//   loadSavedData();
+// });
+
+// function loadSavedData() {
+//   console.log("[app.js] fetching email");
+//   const email = window.localStorage.getItem("email");
+//   if (!email) {
+//     console.error("[app.js] no email found");
+//     return;
+//   }
+//   console.log(`[app.js] ${email}`);
+//   const spanEmail = document.querySelector(".email");
+//   spanEmail.textContent = email;
+// }
+
+// loadSavedData();
