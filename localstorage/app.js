38d245c2d93b9b909b966c344b924464c818b309
paulomatheus <paulomatheus.araujo@hotmail.com>
2025-10-18 10:33:28 -0300

    feat: implement email saving and loading from LocalStorage

--- localstorage/app.js
+++ localstorage/app.js
@@ -1,5 +1,23 @@
 const form = document.querySelector('form');
+const main = document.querySelector('main');
+
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     console.log("[app.js] Form triggered!");
+    console.log("[app.js] email: ", form.email.value);
+    window.localStorage.setItem('email', form.email.value);
+    console.log("[app.js] Email saved to LocalStorage.");
 });
+
+function loadSavedEmail() {
+    console.log("[app.js] Loading saved email from LocalStorage...");
+    const email = window.localStorage.getItem('email');
+    if (email) {
+        console.log("[app.js] Found saved email:", email);
+        form.email.value = email;
+    } else {
+        console.log("[app.js] No saved email found in LocalStorage.");
+    }
+}
+
+loadSavedEmail();
