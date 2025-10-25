51f68a8be0f8cfaaabc425cc856c304f79b49407
paulomatheus <paulomatheus.araujo@hotmail.com>
2025-10-25 11:16:18 -0300

    feat: integrate with github api and implement a card

--- cache-api/app.js
+++ cache-api/app.js
@@ -2,11 +2,9 @@ const button = document.querySelector("button");
 
 button.addEventListener("click", async () => {
   console.log(`👁️ [app.js] button clicked`);
-//fetchFromNetwork("https://api.github.com/users/{user}
-//fetchFromNetwork("https://nonexist/api/v2/pokemon/150");
-//fetchFromNetwork("https://httpbin.org/status/500");
-  const randomId = Math.floor(Math.random() * 151) + 1;
-  const url = `https://pokeapi.co/api/v2/pokemon/${randomId}`;
+  // const randomId = Math.floor(Math.random() * 151) + 1;
+  // const url = `https://pokeapi.co/api/v2/pokemon/${randomId}`;
+  const url = `https://api.github.com/users/edysegura`;
   const data = await fetchData(url);
   showData(data);
 });
@@ -17,7 +15,7 @@ async function fetchData(url) {
 }
 
 async function fetchFromNetwork(url) {
-  console.log(`👁️ [app.js] fetching data from network...`);
+  console.log(`👁️ [app.js] attempt to fetch data from network...`);
   try {
     const response = await fetch(url);
     if (!response.ok) {
@@ -31,6 +29,7 @@ async function fetchFromNetwork(url) {
     //   `👁️ [app.js] response content-type`,
     //   response.headers.get("content-type")
     // );
+    console.log(`👁️ [app.js] request fetched successfully`);
     return response.json();
   } catch (error) {
     console.error(`👁️ [app.js] network failed`);
@@ -50,11 +49,36 @@ async function fetchFromCache(url) {
 async function addToCache(key, response) {
   const cache = await caches.open("MY-CACHE-KEY");
   cache.put(key, response);
+  console.log(`👁️ [app.js] request ${key} was successfully added to cache`);
 }
 
 function showData(data) {
   console.log(`👁️ [app.js] show data`, data);
-  const img = document.createElement("img");
-  img.src = data.sprites.front_default;
-  document.body.appendChild(img);
+  const card = document.createElement("article");
+  card.classList.add("profile-card");
+
+  card.innerHTML = `
+    <header>
+      <img src="${data.avatar_url}" alt="${
+    data.name
+  }" style="border-radius: 50%" width="100" />
+      <h3>${data.name}</h3>
+    </header>
+    <p>${data.bio || "No bio available"}</p>
+    <footer>
+      <strong>Followers:</strong> ${data.followers}
+    </footer>
+  `;
+
+  // Remove previous card if exists
+  const existingCard = document.querySelector(".profile-card");
+  if (existingCard) {
+    existingCard.remove();
+  }
+
+  document.body.appendChild(card);
 }
+
+//fetchFromNetwork("https://api.github.com/users/{user}
+//fetchFromNetwork("https://nonexist/api/v2/pokemon/150");
+//fetchFromNetwork("https://httpbin.org/status/500");
